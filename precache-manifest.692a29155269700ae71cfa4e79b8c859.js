b7a845a0b06c975db8d370f05d3d276c92153926
EricHeredia <EricDavidHeredia@gmail.com>
2018-12-23 13:30:01 -0600

    Updates

---
+++ precache-manifest.692a29155269700ae71cfa4e79b8c859.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3399999d14c61ad309c6",
+    "url": "/drum-machine/static/js/runtime~main.3399999d.js"
+  },
+  {
+    "revision": "35ea271e89ade7f423ef",
+    "url": "/drum-machine/static/js/main.35ea271e.chunk.js"
+  },
+  {
+    "revision": "77958300264c6869a197",
+    "url": "/drum-machine/static/js/1.77958300.chunk.js"
+  },
+  {
+    "revision": "35ea271e89ade7f423ef",
+    "url": "/drum-machine/static/css/main.187c5bc9.chunk.css"
+  },
+  {
+    "revision": "0e232e16265b6044d1c890462ff8df1c",
+    "url": "/drum-machine/index.html"
+  }
+];
